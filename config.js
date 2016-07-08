f42ec0db51eeab9dd3c41e573e03b74eac03594b
Prabhu Subramanian <prabhu@colearnr.com>
2016-07-08 22:24:43 +0100

    Added more config

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 module.exports = {
   extra_art: true,
-  custom_font: 'learnbees'
+  custom_font: 'learnbees',
+  color_lbit_highlight: '#54573A',
+  color_lbit_edit_bg: 'rgba(0, 0, 0, 0.8)',
+  color_primary: 'rgb(211, 84, 0)',
+  lbit_edit_height: '200px',
+  lbit_edit_max_height: '210px'
 }
